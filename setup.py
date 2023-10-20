fbfd6f9b61af4268b83b4ba05331f100f3620fdb
Monica Domaracki <139937885+doma0026@users.noreply.github.com>
2023-10-20 16:09:31 +1030

    services data

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.LLAW33012023S2FWCL1',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.LLAW33012023S2FWCL1\n\nA docassemble extension.\n\n## Author\n\nMonica Domaracki, doma0026@flinders.edu.au\n\n',
+      long_description='# docassemble.LLAW33012023S2FWCL1\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMonica Domaracki, doma0026@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Monica Domaracki',
       author_email='doma0026@flinders.edu.au',
